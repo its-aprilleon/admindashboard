b10a1a81a07101aeb012cb11c5b43325dfd5c6ce
April <thethtaryeem@gmail.com>
2024-05-31 14:58:26 +0630

    added line chart in earning area section

--- js/app.js
+++ js/app.js
@@ -35,6 +35,33 @@ $(document).ready(function () {
     // end navbar
 });
 
+// start earning area
+
+google.charts.load('current', { packages: ['corechart'] });
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+    var data = google.visualization.arrayToDataTable([
+        ['Year', 'Sales', 'Expenses'],
+        ['2004', 1000, 400],
+        ['2005', 1170, 460],
+        ['2006', 660, 1120],
+        ['2007', 1030, 540],
+    ]);
+
+    var options = {
+        title: 'Sales Performance',
+        curveType: 'function',
+        legend: { position: 'bottom' },
+    };
+
+    var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+
+    chart.draw(data, options);
+}
+
+// end earning area
+
 // Start footer
 
 const getyear = document.querySelector('#getyear');
